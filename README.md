0073a2a6ec15552ccb418a9077c3193a50217cbd
“rehithk” <rehith.k@gmail.com>
2023-10-04 19:35:41 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# This GitHub finder uses GITHUB's API. User can find gitHub user's and view their repos.Built using ReactJS and Tailwind.
+
+# To start the Development server 
+* `npm install`
+* `npm start`
---
+++ src/context/github/GithubContext.js
@@ -0,0 +1,86 @@
+import { createContext, useReducer } from "react";
+import githubReducer from "./GithubReducer";
+
+const GithubContext = createContext();
+
+const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
+
+export const GithubProvider = ({ children }) => {
+  const initialState = {
+    users: [],
+    user: [],
+    repos: [],
+    loading: false,
+  };
+
+  const [state, dispatch] = useReducer(githubReducer, initialState);
+
+  const clearUsers = () => {
+    dispatch({
+      type: "CLEAR_USERS",
+    });
+  };
+
+  //Get search users..
+  const searchUsers = async (text) => {
+    setLoading();
+    const params = new URLSearchParams({
+      q: text,
+    });
+    const response = await fetch(`${GITHUB_URL}/search/users?${params}`);
+
+    const { items } = await response.json();
+
+    dispatch({
+      type: "GET_USERS",
+      payload: items,
+    });
+  };
+
+  //Get single User
+  const getUser = async (login) => {
+    setLoading();
+    const response = await fetch(`${GITHUB_URL}/users/${login}`);
+
+    const data = await response.json();
+
+    dispatch({
+      type: "GET_USER",
+      payload: data,
+    });
+  };
+
+  //Get user repos..
+  const getUserRepos = async (login) => {
+    setLoading();
+
+    const response = await fetch(`${GITHUB_URL}/users/${login}/repos`);
+
+    const data = await response.json();
+
+    dispatch({
+      type: "GET_REPOS",
+      payload: data,
+    });
+  };
+  const setLoading = () => dispatch({ type: "SET_LOADING" });
+
+  return (
+    <GithubContext.Provider
+      value={{
+        users: state.users,
+        user: state.user,
+        loading: state.loading,
+        repos: state.repos,
+        searchUsers,
+        clearUsers,
+        getUser,
+        getUserRepos,
+      }}
+    >
+      {children}
+    </GithubContext.Provider>
+  );
+};
+
+export default GithubContext;
